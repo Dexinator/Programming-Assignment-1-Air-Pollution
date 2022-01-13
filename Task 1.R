9b787ed4dd177352e18e643afd87cd7de13ef543
Jorge Badillo <badillo.delgado.jorge@gmail.com>
2022-01-13 15:44:20 -0600

    Modified formnom function to work with vectors

--- Task 1.R
+++ Task 1.R
@@ -1,13 +1,35 @@
 formnom<-function(num){
-  if (nchar(num)==1) {
-    return( paste("00",as.character(num),sep = ""))
-  }else if (nchar(num)==2){
-    return( paste("0",as.character(num),sep = ""))
+  corr_number<-c()
+  for (j in num){
+  if (nchar(j)==1) {
+    corr_number<-append(corr_number,paste("00",as.character(j),sep = ""))
+  }else if (nchar(j)==2){
+    corr_number<-append(corr_number,paste("0",as.character(j),sep = ""))
   }else{
-    return (as.character(num))
+    corr_number<-append(corr_number,as.character(j))
   }
-  
+  }
+  return(corr_number)
 }
+
+
+
+
+
+corr_number
+chartest<-c("hola", "a", "hambe")
+nchar(chartest)==1
+ij<-0
+for (i in chartest){
+    ij<-ij+1
+}
+num3<-c()
+
+append(num3,"500")
+num2<-c(5,2,30)
+longnum2<-length(num2)
+longnum2
+formnom(num2)
 
 
 x<-'A:/Downloads/rprog_data_specdata/specdata/'
@@ -25,3 +47,4 @@ pollutantmean<-function(directory='A:/Downloads/rprog_data_specdata/specdata/',p
   }
 }
 pollutantmean(pollutant = "nitrate",id=5)
+
